1699694a5e026248c92555f911f0963ee9a8e806
Sofiotheque <38701843+Sofiotheque@users.noreply.github.com>
2020-01-09 00:18:40 +0100

    Update audiencerun.js

--- audiencerun.js
+++ audiencerun.js
@@ -2,4 +2,5 @@
 (function(){var a=document.createElement("script"),b=document.getElementsByTagName("script")[0]; a.src="https://d.audiencerun.com/c/10e6tnh82i?d"+(new Date).getTime()+"&r="; try{a.src+=encodeURIComponent(top.document.referrer)}catch(c){a.src+=encodeURIComponent(document.referrer)}a.type="text/javascript"; a.async=!0;b.parentNode.insertBefore(a,b)})(); 
 //970x250 audiencerun
 (function(){var a=document.createElement("script"),b=document.getElementsByTagName("script")[0]; a.src="https://d.audiencerun.com/c/ifr1xhe05z?d"+(new Date).getTime()+"&r="; try{a.src+=encodeURIComponent(top.document.referrer)}catch(c){a.src+=encodeURIComponent(document.referrer)}a.type="text/javascript"; a.async=!0;b.parentNode.insertBefore(a,b)})();
-© 2020 GitHub, Inc
+//Search Button
+$(function(){$(".searchbutton").click(function(){$(this).toggleClass("active"),$(".search-form").slideToggle("fast")})});
