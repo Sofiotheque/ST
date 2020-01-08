bdd925282ec27e43a2f7262586899e1850b3b6c6
Sofiotheque <38701843+Sofiotheque@users.noreply.github.com>
2020-01-09 00:16:13 +0100

    Create audiencerun.js

---
+++ audiencerun.js
@@ -0,0 +1,5 @@
+//Footer Fix audiencerun
+(function(){var a=document.createElement("script"),b=document.getElementsByTagName("script")[0]; a.src="https://d.audiencerun.com/c/10e6tnh82i?d"+(new Date).getTime()+"&r="; try{a.src+=encodeURIComponent(top.document.referrer)}catch(c){a.src+=encodeURIComponent(document.referrer)}a.type="text/javascript"; a.async=!0;b.parentNode.insertBefore(a,b)})(); 
+//970x250 audiencerun
+(function(){var a=document.createElement("script"),b=document.getElementsByTagName("script")[0]; a.src="https://d.audiencerun.com/c/ifr1xhe05z?d"+(new Date).getTime()+"&r="; try{a.src+=encodeURIComponent(top.document.referrer)}catch(c){a.src+=encodeURIComponent(document.referrer)}a.type="text/javascript"; a.async=!0;b.parentNode.insertBefore(a,b)})();
+© 2020 GitHub, Inc
